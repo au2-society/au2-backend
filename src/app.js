e2533096678c30882bc3e05e12ba625d7c24f271
Mukul Anand <mukulanand.biz@gmail.com>
2025-02-20 18:21:32 +0530

    Added CORS to allow all requests

--- src/app.js
+++ src/app.js
@@ -25,23 +25,31 @@ if (NODE_ENV === "production") {
   app.set("trust proxy", 1);
 }
 
-const allowedOrigins = CORS_ORIGIN ? CORS_ORIGIN.split(",") : [];
+// const allowedOrigins = CORS_ORIGIN ? CORS_ORIGIN.split(",") : [];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       } else {
+//         const msg =
+//           "The CORS policy for this site does not allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
